37c9ae6f14f49327735a1c118284f355708ad14f
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-05-02 15:55:21 -0300

    Criação da classe CorrentistaConta para testes.

--- AppBancoDigital/AppBancoDigital/Service/DataServiceCorrentista.cs
+++ AppBancoDigital/AppBancoDigital/Service/DataServiceCorrentista.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using AppBancoDigital.Model;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace AppBancoDigital.Service
 {
     public class DataServiceCorrentista : DataService 
     {
-
+        
     }
 }
