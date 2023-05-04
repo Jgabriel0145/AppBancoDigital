1a298ea418c9eede28f17db521df2f2ed143cdc5
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-05-04 15:52:03 -0300

    Métodos da DataServiceCorrentista.

--- AppBancoDigital/AppBancoDigital/Model/Correntista.cs
+++ AppBancoDigital/AppBancoDigital/Model/Correntista.cs
@@ -6,6 +6,11 @@ namespace AppBancoDigital.Model
 {
     public class Correntista
     {
-
+        public string Nome { get; set; }
+        public int Cpf { get; set; }
+        public DateTime Data_Nasc { get; set; }
+        public string Senha { get; set; }
+        public bool Ativo { get; set; }
+        public int Id_conta { get; set; }
     }
 }
--- AppBancoDigital/AppBancoDigital/Service/DataServiceCorrentista.cs
+++ AppBancoDigital/AppBancoDigital/Service/DataServiceCorrentista.cs
@@ -1,14 +1,56 @@
-﻿using AppBancoDigital.Model;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
+using AppBancoDigital.Model;
+
 namespace AppBancoDigital.Service
 {
-    public class DataServiceCorrentista : DataService 
+    public class DataServiceCorrentista : DataService
     {
-        
+        public static async Task<List<Correntista>> GetCorrentistaAsync()
+        {
+            string json = await GetDataFromService("/correntista");
+
+            List<Correntista> list_correntista = JsonConvert.DeserializeObject<List<Correntista>>(json);
+
+            return list_correntista;
+        }
+
+        public static async Task<Correntista> CadastrarCorrentista(Correntista correntista)
+        {
+            var json_para_enviar = JsonConvert.SerializeObject(correntista);
+
+            string json = await PostDataToService(json_para_enviar, "/correntista/save");
+
+            Correntista correntista2 = JsonConvert.DeserializeObject<Correntista>(json);
+
+            return correntista2;
+        }
+
+        public static async Task<List<Correntista>> SearchAsync(string query)
+        {
+            var json_para_enviar = JsonConvert.SerializeObject(query);
+
+            string json = await PostDataToService(json_para_enviar, "/correntista/search");
+
+            List<Correntista> list_correntista = JsonConvert.DeserializeObject<List<Correntista>>(json);
+
+            return list_correntista;
+        }
+
+        public static async Task<List<Correntista>> DeleteAsync(int id)
+        {
+            var json_para_enviar = JsonConvert.SerializeObject(id);
+
+            string json = await PostDataToService(json_para_enviar, "/correntista/delete");
+
+            List<Correntista> list_correntista = JsonConvert.DeserializeObject<List<Correntista>>(json);
+
+            return list_correntista;
+        }
     }
 }
