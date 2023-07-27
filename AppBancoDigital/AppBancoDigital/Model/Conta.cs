bccc3a22d879e35a34a3a8cd49384845a4cbc7f3
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-07-27 15:59:20 -0300

    Criação de contas.

--- AppBancoDigital/AppBancoDigital/Model/Conta.cs
+++ AppBancoDigital/AppBancoDigital/Model/Conta.cs
@@ -6,11 +6,10 @@ namespace AppBancoDigital.Model
 {
     public class Conta
     {
-        public int Id { get; set; }
-        public string Tipo { get; set; }
-        public double Saldo { get; set; }
-        public double Limite { get; set; }
-        public DateTime Data_Abertura { get; set; }
-        public Correntista correntista { get; set; }
+        public int id { get; set; }
+        public string tipo { get; set; }
+        public double saldo { get; set; }
+        public double limite { get; set; }
+        public Correntista id_correntista { get; set; }
     }
 }
