b2c5d06bbf6f8c495234cbde9f107ea4b69d298a
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2023-09-26 15:53:02 -0300

    Ler QR Code.

---
+++ AppBancoDigital/AppBancoDigital/View/Pix/LerQrCode.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppBancoDigital.View.Pix
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LerQrCode : ContentPage
+	{
+		public LerQrCode()
+		{
+			InitializeComponent();
+
+            zxing.OnScanResult += (result) => Device.BeginInvokeOnMainThread(() =>
+            {
+                lbl_result.Text = result.Text;
+            });
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            zxing.IsScanning = true;
+        }
+
+        protected override void OnDisappearing()
+        {
+            zxing.IsScanning = false;
+
+            base.OnDisappearing();
+        }
+    }
+}
